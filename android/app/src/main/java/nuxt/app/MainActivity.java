8d1fe4828e969dc8c44e579eb859a4141a37a485
end3r-man <praveen.venkatachalam@proton.me>
2024-09-21 23:53:38 +0530

    🫥 Re-Init

---
+++ android/app/src/main/java/nuxt/app/MainActivity.java
@@ -0,0 +1,5 @@
+package nuxt.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
+
+
+<script setup>
+import { StatusBar } from '@capacitor/status-bar';
+StatusBar.setOverlaysWebView({ overlay: true });
+</script>
